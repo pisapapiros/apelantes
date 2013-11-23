31a22c5ef873da80b19bf7eeca8d83145bc027a8
Pablo <pcamino90@gmail.com>
2013-11-23 14:55:50 +0100

    Primera beta
    Ya funcionan las tildes, y además hay una pequeña guía de uso

--- cartas.py
+++ cartas.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -⁻- coding: UTF-8 -*-
 #Requiere tener instalado python y la libreria de reportlab
+#Pablo Camino Bueno, AIUS, 2013
 
 from Tkinter import *
 from reportlab.lib.pagesizes import letter
@@ -9,6 +10,7 @@ from reportlab.lib.units import mm, inch
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Paragraph, Table
 import codecs
+import os.path
 
 
 #======================CLASES===================
@@ -33,6 +35,9 @@ class LetterMaker(object):
         table.drawOn(self.c, *self.coord(18, 60, mm))
 
         # insert body of letter
+        print "bodyyy"
+        print body
+        body = body.replace('\n','<br />')
         p = Paragraph(body, self.styles["Normal"])
         p.wrapOn(self.c, self.width-70, self.height)
         p.drawOn(self.c, *self.coord(20, voffset+50, mm))
@@ -86,20 +91,34 @@ def textoEnMedio(sCadena, sDelimitador1, sDelimitador2):
 def comenzar(rutaApelantes,rutaCarta):
     apelantes = crear_apelantes(rutaApelantes)
     cuerpo1 = crear_cuerpo1(rutaCarta)
+    #print cuerpo1
     cuerpo2 = crear_cuerpo2(rutaCarta)
     autoridades = crear_autoridades(rutaCarta)
     contadorCartas1 = 0
     contadorCartas2 = 0
+    #print "Todos los apelantes:"
+    #for apelante in apelantes:
+    #    print apelante.nombre
+    #print "_____________________"
+    #print "Todas las autoridades:"
+    #for autoridad in autoridades:
+    #    print autoridad.nombre
+    #print "_____________________"
     for apelante in apelantes:
+    #    print "iteracion del for apelantes"
         crear_carta(apelante,cuerpo2,"",contadorCartas2)
+    #    print "carta creada"
         contadorCartas2 = contadorCartas2+1
+        #print "autoridades"
         for autoridad in autoridades:
             contadorCartas1 = contadorCartas1+1
+            #print "vamos a crear carta"
             crear_carta(autoridad,cuerpo1,apelante,contadorCartas1)
+            #print "creada!"
     res.set("Hecho, la lucha continúa!")
 		
 def crear_carta(destinatario,cuerpo,cierre, contadorCartas):
-    doc = LetterMaker("carta"+str(contadorCartas)+".pdf", "The MVP", 10)
+    doc = LetterMaker("carta"+str(contadorCartas)+".pdf", "AI", 10)
     doc.createDocument(destinatario, cuerpo, cierre)
     doc.savePDF()
 
@@ -120,71 +139,90 @@ def crear_apelantes(rutaApelantes):
     return apelantes
 
 def crear_autoridades(rutaCarta):
+    #print "entro a crear_autoridades"
     ficheroCarta = open(rutaCarta, 'r')
-    autoridadesAPelo =  textoEnMedio(str(ficheroCarta.readlines()), "#Autoridades\n", "#Cuerpo1")
+    texto=""
+    for line in ficheroCarta.readlines():
+        texto=texto+line
+    autoridadesAPelo =  textoEnMedio(texto, "#Destinatarios\n", "#Cuerpo1")
+    #print autoridadesAPelo
     ficheroCarta.close()
-    x=1
+    x=0
     autoridades=[]
-    autoridadesRetocadas = autoridadesAPelo.split("\\n")
-    for i in range((len(autoridadesRetocadas)/6)):
-        nombre = autoridadesRetocadas[0+x]
-        direccion1 = autoridadesRetocadas[1+x]
-        direccion2 = autoridadesRetocadas[2+x]
-        direccion3 = autoridadesRetocadas[3+x]
-        direccion4 = autoridadesRetocadas[4+x]
+    autoridadesRetocadas2=[]
+    autoridadesRetocadas3=[]
+    autoridadesRetocadas = autoridadesAPelo.split("#")
+    for a in autoridadesRetocadas:
+        autoridadesRetocadas2.append(a.split('\n'))
+    #print autoridadesRetocadas2
+    #print "__________________"
+    for x in autoridadesRetocadas2:
+    #    print x
+    #    print
+        autoridadesRetocadas3.append([elem for elem in x if elem !=""])
+    #print "autoridadesRetocadas3"
+    #for a in autoridadesRetocadas3:
+        #print
+        #print a
+    for a in autoridadesRetocadas3:
+        nombre = a[0]
+        direccion1 = a[1]
+        direccion2 = a[2]
+        direccion3 = a[3]
+        direccion4 = a[4]
         autoridades.append(Persona(nombre, direccion1, direccion2, direccion3, direccion4))
-        x=x+6
+    #print "aqui vienen las autoridades"
     return autoridades
 
 def crear_cuerpo1(rutaCarta):
+    #print "creo cuerpo1"
     ficheroCarta = codecs.open(rutaCarta, "r", encoding='utf-8')
     #buscar cuerpo1, entonces leer hasta cuerpo2
-    cuerpo =textoEnMedio(str(ficheroCarta.readlines()), "#Cuerpo1", "#Cuerpo2")
-    print cuerpo
+    texto=""
+    for line in ficheroCarta.readlines():
+        texto=texto+line
+    cuerpo =textoEnMedio(texto, "#Cuerpo1", "#Cuerpo2")
+    ficheroCarta.close()
+    #print cuerpo
     return cuerpo
 
 def crear_cuerpo2(rutaCarta):
+    #print "creo cuerpo2"
     ficheroCarta = open(rutaCarta, "r")
-    cuerpo = textoEnMedio(str(ficheroCarta.readlines()), "#Cuerpo2", "#Fin")
+    texto=""
+    for line in ficheroCarta.readlines():
+        texto=texto+line
+    cuerpo = textoEnMedio(texto, "#Cuerpo2", "#Fin")#.decode('utf8')
+    ficheroCarta.close()
     return cuerpo
 
-
 #=======================MAIN=======================
-
 #ventana principal
 master = Tk()
 frame = Frame(master, height=3200, width=80)
 frame.pack()
 
-
 #caja de texto de la ruta 1
 rutaApelantes = StringVar()
 textRutaApelantes = Entry(frame, textvariable=rutaApelantes, width=50)
 textRutaApelantes.pack(side=LEFT)
-#textRutaApelantes.insert(0, "Ruta completa del fichero de apelantes")
-textRutaApelantes.insert(0, "C:/Users/Pablo/Dropbox/apuntes/AI Deberes/apelantes/apelantes.txt")
+textRutaApelantes.insert(0, os.path.join(os.path.dirname(__file__), 'apelantes.txt').replace('\\','/'))
 
 #caja de texto de la ruta 2
 rutaCarta = StringVar()
 textRutaCarta = Entry(frame, textvariable=rutaCarta, width=50)
 textRutaCarta.pack(side=LEFT)
-#textRutaCarta.insert(0, "Ruta completa de la carta")
-textRutaCarta.insert(0, "C:/Users/Pablo/Dropbox/apuntes/AI Deberes/apelantes/cartas.txt")
+textRutaCarta.insert(0, os.path.join(os.path.dirname(__file__), 'cartas.txt').replace('\\','/'))
 
 #boton
 accion = lambda: comenzar(rutaApelantes.get(), rutaCarta.get())
 button = Button(frame, text="Crear cartas", command=accion)
 button.pack(side=RIGHT)
 
-
 #etiqueta para mostrar resultado
 res = StringVar()
 Label(frame, textvariable=res).pack()
 res.set("Sin Empezar")
 
-
 #para que se mantenga el programa sin cerrarse
 frame.mainloop()
-
-
-
